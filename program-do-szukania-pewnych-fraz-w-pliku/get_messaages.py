65564123caff33efca8f18447af25bdd26ec6b73
marcinlisek <94200668+marcinlisek@users.noreply.github.com>
2021-11-30 23:27:27 +0100

    rewrite code to allow assert usage

--- program-do-szukania-pewnych-fraz-w-pliku/get_messaages.py
+++ program-do-szukania-pewnych-fraz-w-pliku/get_messaages.py
@@ -1,8 +1,31 @@
-messages = open("messages.txt")
+input_messages = open("test input.txt")
 
-read_messages = messages.read()
+read_input = input_messages.read()
 
-lines = read_messages.split("\n")
+output_messages = open("test output.txt")
+
+read_output = output_messages.read()
+
+
+def main(read_input, adr):
+
+    lines = read_input.split("\n")
+
+    adr_idx = get_idx(lines, adr)
+
+    filtered_messages = []
+
+    for idx in adr_idx:
+        filtered_messages.append(get_messages(idx, lines))
+
+    joined_messages = []
+
+    for message in filtered_messages:
+        joined_messages.append("\n".join(message))
+
+    output = "\n\n".join(joined_messages)
+
+    return output
 
 
 def get_idx(lines, adr):
@@ -25,13 +48,4 @@ def get_messages(idx, lines):
     return messages_list
 
 
-adr_idx = get_idx(lines, "#penzu")
-
-filtered_messages = []
-
-for idx in adr_idx:
-    filtered_messages.append(get_messages(idx, lines))
-
-for message in filtered_messages:
-    print(" ".join(message))
-    print(" ")
+assert main(read_input, "#penzu") == read_output
